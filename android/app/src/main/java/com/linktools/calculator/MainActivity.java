0d252f9b0433ce51b2c3f0a178efafff233c3e21
Kevin <zess1982@gmail.com>
2026-03-13 23:43:21 +0800

    更新图标。

---
+++ android/app/src/main/java/com/linktools/calculator/MainActivity.java
@@ -0,0 +1,5 @@
+package com.linktools.calculator;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
 import { I18nProvider } from './contexts/I18nContext'
 import App from './App'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <I18nProvider>
           <App />
         </I18nProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
